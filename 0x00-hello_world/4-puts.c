5766e4f34ec753224d73ec983b31ee831e13f8ac
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-29 08:01:43 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description : prints "programming is like building a multilingual puzzle"
+ */
+
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzle");
+
+	return (0);
+}
