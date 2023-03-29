fab410a732809d55858e3c7c91f70114a2ad051f
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-29 10:00:38 -0700

    Task 8. UNIX is basically a simple operating system, but you have to be genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Not allowed to use any functions listed in the NAME section of the man (3)
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
