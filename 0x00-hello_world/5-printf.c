c5ad3c7ad8cdec289e89427b0788d6d36c946f48
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-29 08:18:15 -0700

    Task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * Using the function printf
+ * Not allowed to use the function puts
+ * Return : 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a place of art,"\n);
+
+	return (0);
+}
