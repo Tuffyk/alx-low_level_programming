4b6e26eef6077e30e271ad37bb3f21ebd1514750
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-30 08:38:16 -0700

    Task 1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,10 @@
 
 /**
  * main - prints the last digit of the number stored in the variable n
+ * It also tells if the last digit is :
+ * greater than 5
+ * is 0
+ * less than 6 and not 0
  * Return: 0
  */
 
@@ -17,17 +21,18 @@ int main(void)
 
 	last_digit = n % 10;
 
-	if (last_digit>5)
+	if (last_digit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
-	else if (last_digit==0)
+	else if (last_digit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6
+			       	and not 0\n", n, last_digit);
 	}
 	return (0);
 }
