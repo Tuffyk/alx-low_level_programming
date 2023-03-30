e9d4cf430cb533fe1a3b2748065e7c56c8431ec6
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-30 11:26:41 -0700

    Task 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <sdtio.h>
+
+/**
+ * * main - Prints all single numbers of base 10 starting from 0
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		++n;
+	}
+	
+	printf("\n");
+
+	return (0);
+}
