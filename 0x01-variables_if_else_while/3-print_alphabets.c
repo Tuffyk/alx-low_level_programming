883547e510406695e915ad341ca4bff583196d2a
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-30 10:57:26 -0700

    Task 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then uppercase
+ * followed by a new line
+ * Only using the putchar function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
