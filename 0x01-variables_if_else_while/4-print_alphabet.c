33cdacfae53726ce5fbdefd5cd7799b212698f6e
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-30 11:09:49 -0700

    Task 4. When I was having that alphabet soup, I never thought that I would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ * Prints all letters except q and e
+ * only using the putchar function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	do{
+		putchar(c);
+		c++;
+	}while (c <= 'z' && c != 'e' && c!= 'q');
+
+	putchar('\n');
+
+	return (0);
+}	
