8d663517e0afcd438b910b9e815c03213e02bf7e
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-31 03:07:30 -0700

    Task 9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers
+ * Numbers must be separated by , followed by a space
+ * Numbers should be printed in ascending order
+ * Only using the putchar function
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		putchar('0' + n);
+		putchar(", ");
+	}
+	putchar("\$\n");
+}
+
