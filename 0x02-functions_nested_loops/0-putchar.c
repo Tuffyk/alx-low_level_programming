289739cf11ae3aa481baaa17b5f8075d001b6110
Taoufik Malki <taoufik.malki@gmail.com>
2023-03-31 10:17:38 -0700

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,4 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On errot, -1 is returned, and errno is set appropriatly.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include "_putchar.h"
 
 /**
  * main - Prints _putchar, followed by a new line
