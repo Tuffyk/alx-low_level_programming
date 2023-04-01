016fa55d49390fb416f1ecb03e570fd0ff5df938
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-01 01:09:31 -0700

    Task 8. I am a federal agen Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day starting from 00:00 to 23:59
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0 ; hours <= 23 ; hours++)
+	{
+		for (minutes = 0 ; minutes <= 59 ; minutes++)
+		{
+			if (hours == 0 || minutes == 0)
+			{
+				_putchar('0' + hours);
+				_putchar('0' + hours);
+				_putchar(':');
+				_putchar('0' + minutes);
+				-putchar('0' + minutes);
+				_putchar('\n');
+			}
+			_putchar('0' + hours);
+			_putchar(':');
+			_putchar('0' + minutes);
+			_putchar('\n');
+		}
+	}
+	
+	return (0);
+}
