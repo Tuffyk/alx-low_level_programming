cd98268f05f8ac8a42a91bbd403be9e4f06cd83a
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-01 02:06:08 -0700

    Task 8. I am a federal agen Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,18 +13,22 @@ void jack_bauer(void)
 	{
 		for (h2 = 0 ; h2 <= 9 ; h2++)
 		{
-			for (m1 = 0 ; m1 <= 5 ; m1++)
+			if (h1 <= 1 && h2 <= 9 || h1 <= 2 && h2 <= 3)
 			{
-				for (m2 = 0 ; m2 <= 9 ; m2++)
+				for (m1 = 0 ; m1 <= 5 ; m1++)
 				{
-					_putchar('0' + h1);
-					_putchar('0' + h2);
-					_putchar(':');
-					_putchar('0' + m1);
-					_putchar('0' + m2);
-					_putchar('\n');
+					for (m2 = 0 ; m2 <= 9 ; m2++)
+					{
+						_putchar('0' + h1);
+						_putchar('0' + h2);
+						_putchar(':');
+						_putchar('0' + m1);
+						_putchar('0' + m2);
+						_putchar('\n');
+					}
 				}
 			}
 		}
 	}
 }
+
