66f7cd2fe087f5b7d6a7d7786fc05c093071a2e0
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-01 00:06:13 -0700

    Task 6. There is no such thing as absolute value in this world.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,16 +3,21 @@
 /**
  * _abs - Computes the absolute value of an integer
  * @n : the number to be checked
- * Return : absolute value of the number or zero
+ * Return : 0
  */
-
 int _abs(int n)
 {
 	if (n < 0)
 	{
 		int abs;
+		
 		abs = n * -1;
 		return (abs);
 	}
-	return (n);
+	else
+	{
+		return (n);
+	}
+
+	return (0);
 }
