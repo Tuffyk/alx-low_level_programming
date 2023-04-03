0266e609f28f7bd2ffdd0f1d3cbdd77f1555ac3a
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-02 17:08:03 -0700

    Task 7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,30 +8,15 @@
 
 void puts_half(char *str)
 {
-	int i, j;
+	int i;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 	}
-	if ((i + 1) % 2 == 0)
+	i++;
+	for (i /= 2; str[i] != '\0' ; i++)
 	{
-		for (j = 0 ; j <= (i + 1) ; j++)
-		{
-			if (j > i / 2)
-			{
-				_putchar(str[j]);
-			}
-		}
-	}
-	if ((i - 1) % 2 == 1)
-	{
-		for (j = 0; j < (i - 1) ; j++)
-		{
-			if (j > (i - 1) / 2)
-			{
-				_putchar(str[j]);
-			}
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
