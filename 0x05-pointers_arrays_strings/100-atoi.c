1c72e163f61608bb3bdf563f689cf6d973430a51
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-04 14:11:16 -0700

    Task 10. Great leaders

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: pointer to string
+ * Return: converted string
+ */
+
+int _atoi(char *s)
+{
+	int i, j, totalMin, length;
+	
+	totalMin = 0;
+	length = 0
+
+	i = 0;
+	while (*s != '\0')
+	{
+		if ((*(s + i)) == '-')
+		{
+			totalMin++;
+		}
+		i++;
+	}
+	if ( totalMin % 2 == 1)
+	{
+		*(s + 0) = '-';
+		length++;
+
+		for (j = 1 ; *s != '\0'; j++)
+		{
+			if (*(s + j) >= 48 && *(s + j) <= 57)
+			{
+				*(s + lenght) = *(s + j);
+				length++;
+			}
+		}
+	}
+	else
+	{
+		for (j = 0; *s != '\0' ; j++)
+		{
+			if (*(s + j) >= 48 && *(s + j) <= 57)
+			{
+				*(s + length) = *(s + j);
+				length++;
+			}
+		}
+	}
+	*(s + length) = '\0';
+
+	return (*s);
+}
