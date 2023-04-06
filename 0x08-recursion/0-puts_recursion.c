bb7ae32193aa103a0dccf585f5cc5d68b2126028
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-06 11:47:20 +0000

    Task 0. She locked away a secret

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	_putchar(s[i]);
+	_putchar(*(s + i));
 	i++;
 
 	if (*s != '\0')
 	{
-		_puts_recursion(s[i]);
+		_puts_recursion(*(s + i));
 	}
 }
