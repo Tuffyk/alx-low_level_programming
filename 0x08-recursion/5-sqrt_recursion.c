3a9df852a6f3c595dc8f00b7621aad6ea279f377
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-07 09:47:42 +0000

    Task 5. Your suconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,6 @@
  * Return: natural square root of n
  */
 
-/**
- * _sqrt_check - Checks for the natural square root of the number
- * @n: computed number
- * @x: number used for checking
- * Return: natural square root of the number
- */
-
 int _sqrt_recursion(int n)
 {
 	int x = 2;
@@ -27,6 +20,13 @@ int _sqrt_recursion(int n)
 		return (_sqrt_check(n, x));
 	}
 }
+
+/**
+ * _sqrt_check - Checks for the natural square root of the number
+ * @n: computed number
+ * @x: number used for checking
+ * Return: natural square root of the number
+ */
 
 int _sqrt_check(int n, int x)
 {
