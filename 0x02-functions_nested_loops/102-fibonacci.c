b2bdea162ec61d0abb0254ec6e4e56aad0a5dd8e
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-08 17:39:17 +0000

    Task 14. 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, n1, n2, sum;
+
+	n1 = 1;
+	n2 = 2;
+	sum = n1 + n2;
+	
+	ptintf("%d, %d, ", n1, n2);
+
+	i = 3;
+	while (i <= 50)
+	{
+		printf("%d, ", sum);
+		n1 = n2;
+		n2 = sum;
+		sum = n1 + n2;
+		i++;
+	}
+	return (0);
+}
