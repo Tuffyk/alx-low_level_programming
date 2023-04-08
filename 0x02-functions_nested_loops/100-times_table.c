72cd8e00ff2974d8870f234336398fa2a726a9f1
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-08 14:19:02 +0000

    Task 12. The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,43 +10,50 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	i = 0;
-	while (i <= n)
+	if (n > 0 && n < 15)
 	{
-		j = 0;
-		while (j < n)
+		i = 0;
+		while (i <= n)
 		{
-			if ((i * j) == 0 && j == 0)
+			j = 0;
+			while (j < n)
 			{
-				_putchar('0');
+				if ((i * j) == 0 && j == 0)
+				{
+					_putchar('0');
+				}
+				j++;
+				if ((i * j) <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (i * j));
+				}
+				else if ((i * j) >= 10 && (i * j) <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + ((i * j) / 10));
+					_putchar('0' + ((i * j) % 10));
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + ((i * j) / 100));
+					_putchar('0' + (((i * j) / 10)) % 10);
+					_putchar('0' + ((i * j) % 10));
+				}
 			}
-			j++;
-			if ((i * j) <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (i * j));
-			}
-			else if ((i * j) >= 10 && (i * j) <= 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + ((i * j) / 10));
-				_putchar('0' + ((i * j) % 10));
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + ((i * j) / 100));
-				_putchar('0' + (((i * j) / 10)) % 10);
-				_putchar('0' + ((i * j) % 10));
-			}
+			i++;
+			_putchar('\n');
 		}
-		i++;
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
