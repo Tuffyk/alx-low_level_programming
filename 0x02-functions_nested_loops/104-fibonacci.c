2092130f645542dd2e63b90be8298e17321fec44
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-08 18:45:57 +0000

    Task 16. Fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,26 +8,26 @@
 int main(void)
 {
 	int i;
-	unsigned double n1, n2, sum;
+	unsigned long n1, n2, sum;
 
 	n1 = 1;
 	n2 = 2;
 	sum = n1 + n2;
-	printf("%.0f, %.0f, ", n1, n2);
+	printf("%ld, %ld, ", n1, n2);
 
 	i = 3;
 	while (i <= 98)
 	{
 		if (i < 98)
 		{
-			printf("%.0f, ", sum);
+			printf("%ld, ", sum);
 			n1 = n2;
 			n2 = sum;
 			sum = n1 + n2;
 		}
 		else
 		{
-			printf("%.0f\n", sum);
+			printf("%ld\n", sum);
 		}
 		i++;
 	}
