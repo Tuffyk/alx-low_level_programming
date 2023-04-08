52ae5c5e611d8c642957b3423fcb1212524b3236
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-08 16:58:30 +0000

    Task 12. The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,34 +22,34 @@ void print_times_table(int n)
 		{
 			j = 0;
 			while (j <= n)
-			{	
+			{
 				if (j > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if ((i * j) > 99)
 					{
-						_putchar(',');
+						_putchar('0' + ((i * j) / 100));
+						_putchar('0' + (((i * j) / 10) % 10));
+						_putchar('0' + ((i * j) % 10));
+					}
+					else if ((i * j) > 9)
+					{
 						_putchar(' ');
-						if ((i * j) > 99)
-						{
-							_putchar('0' + ((i * j) / 100));
-							_putchar('0' + (((i * j) / 10) % 10));
-							_putchar('0' + ((i * j) % 10));
-						}
-						else if ((i * j) > 9)
-						{
-							_putchar(' ');
-							_putchar('0' + ((i * j) / 10));
-							_putchar('0' + (i * j) % 10);
-						}
-						else
-						{
-							_putchar(' ');
-							_putchar(' ');
-							_putchar('0' + (i * j));
-						}
+						_putchar('0' + ((i * j) / 10));
+						_putchar('0' + (i * j) % 10);
 					}
 					else
 					{
-						_putchar('0');
-					}	
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + (i * j));
+					}
+				}
+				else
+				{
+					_putchar('0');
+				}
 				j++;
 			}
 			i++;
