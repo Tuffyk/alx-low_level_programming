1660ce0367a41776a624579beb793ffded87e997
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-10 06:04:29 +0000

    Task 1. writing a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - The programme will print the number of arguments passed into it
+ * followed by a new line
+ * @argc: the count of the arguments
+ * @argv: array of pointers to the strings
+ *
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
