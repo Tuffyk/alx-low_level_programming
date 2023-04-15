16023d4085d6ffab79b222d722eccc26622ba770
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-15 17:30:12 +0000

    Task 3. If you even dream of beating me you would better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,15 +30,16 @@ int **alloc_grid(int width, int height)
 			for (i = 0; i < height; i++)
 			{
 				arr[i] = malloc(sizeof(int) * width);
-			}
-		}
-		for (i = 0; i < height; i++)
-		{
-			for (j = 0; j < width; j++)
+				if (arr[i] = NULL)
+				{
+					return (NULL);
+				}
+				for (j = 0; j < width; j++)
 				{
 					arr[i][j] = 0;
 				}
-		}
+			}
 		return (arr);
+		}
 	}
 }
