3076387dd1a9b343600a33a2414a635a33e5427b
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-16 15:47:55 +0000

    Task 0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - Allocates memory
+ * @b: size of memory to be allocated
+ *
+ * Return: a pointer to the allocated memory on success, otherwise return "98".
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
