08f30ad037b375257368bdceefce75c6dee7d5b1
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-16 17:07:16 +0000

    Task 1. string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *string_nconcat - Concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of first bytes of s2
+ *
+ * Return: pointer to the new string on success, otherwise "NULL"
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, len1, len2;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{
+	}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{
+	}
+	str = malloc(sizeof(char) * (len1 + len2));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			str[i] = s1[i];
+		}
+		if (n < len2)
+		{
+			for (i = 0; i < n; i++)
+			{
+				str[len1 + i] = s2[i];
+			}
+		}
+		else
+		{
+			for (i = 0; s2[i] != '\0'; i++)
+			{
+				str[len1 + i] = s2[i];
+			}
+		}
+		return (str);
+	}
+}
+
