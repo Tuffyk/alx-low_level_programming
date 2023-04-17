11ccfffbfe3752be39f70cf1b046531481c8fb0b
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-17 08:23:37 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,33 +11,25 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *c;
-	int i, j, s1_len, s2_len, c_len;
+	char *str;
+	int len1, len2, i;
 
-	s1_len = 0;
-	s2_len = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{
-		if (s1[i] != '\0')
-			s1_len++;
-		if (s2[i] != '\0')
-			s2_len++;
 	}
-	c_len = s1_len + s2_len;
-	c = malloc(sizeof(char) * c_len + 1);
-	if (c == NULL)
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{
+	}
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (str == NULL)
 		return (NULL);
-	for (j = 0; s1[j] != '\0'; j++)
-	{
-		c[j] = s1[j];
-	}
-	for (j = 0; j <= s2_len; j++)
-	{
-		c[s1_len + j] = s2[j];
-	}
-	return (c);
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	for (i = 0; i <= len2; i++)
+		str[len1 + i] = s2[i];
+	return (str);
 }
