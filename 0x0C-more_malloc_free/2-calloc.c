5feb9a52e2522d35251a588613d32a5ab31c86a7
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-17 07:09:51 +0000

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,8 +16,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
-	return (ptr);
+	else
+	{
+		ptr = malloc(nmemb * size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
 }
