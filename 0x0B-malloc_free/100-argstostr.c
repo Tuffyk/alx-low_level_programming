db966d11138aaac474dfd2fe3d4c5c69ada9a4a9
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-17 11:36:27 +0000

    Task 5. it isn't the mountians ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *argstostr - Concatenates all the arguments of a program
+ * @ac: arguments count
+ * @av: array of strings
+ *
+ * Return: pointer to the new string on success, otherwise "NULL"
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, len;
+	char *str;
+
+	len = 0, k = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; arg[i][j] != '\0'; j++)
+			len++;
+	}
+	str = malloc(sizeof(char) * (len + 8));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		k++;
+		str[k] = '\n';
+	}
+	return (str);
+}
