ff5cbd86d88405f49ace28f6f7307f8b4747d58f
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-17 17:22:15 +0000

    Task 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Poppy
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Poppy information
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
