c3a924ff017c26ca6534a2d30455262de842d397
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-17 09:00:21 +0000

    Task 3. If you even dream of beating me you d better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,11 +27,14 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(sizeof(int) * width);
-		if (arr[i] == NULL)
-		{
-			free(arr[i]);
-			return (NULL);
-		}
+	}
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+	for (i = 0; i< height; i++)
+	{
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
