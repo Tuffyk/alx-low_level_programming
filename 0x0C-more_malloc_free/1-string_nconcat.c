78262060ba2395ab01ef5f58b482ed222cb8169a
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-17 06:56:58 +0000

    Task 1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,9 +32,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			return (NULL);
 		for (i = 0; s1[i] != '\0'; i++)
 			str[i] = s1[i];
-		for (i = 0; i <= n; i++)
-			str[len1 + i] = s2[i];
-		/* str[n + i] = '\0';*/
+		if (n > 0)
+		{
+			for (i = 0; i <= n; i++)
+				str[len1 + i] = s2[i];
+			str[n + i] = '\0';
+		}
+		else
+			str[n] = '\0';
 	}
 	else
 	{
