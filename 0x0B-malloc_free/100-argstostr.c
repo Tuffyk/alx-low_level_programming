bb381be16a44482b141304c4d4ba023159de7d7b
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-17 11:37:23 +0000

    Task 5. it isn't the mountians ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; arg[i][j] != '\0'; j++)
+		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
 	}
 	str = malloc(sizeof(char) * (len + 8));
