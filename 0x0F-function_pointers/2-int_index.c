0059d2f8203b9a3c3a7b9e2328f0d0ea524a0b6a
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-19 14:56:28 +0000

    Task 2. To hell with circumstances; I creat opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return (-1);
+		return;
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
+		if (cmp(array[i]) == "True")
 			return (i);
 	}
 }
