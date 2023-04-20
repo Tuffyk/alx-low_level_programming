fa3d2b26c16e63a1c0c51fa6c7b523214481684b
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-20 16:49:22 +0000

    Task 1. To be is to be the value of a variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
@@ -19,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(ap, char *) == "NULL")
+		if (va_arg(ap, char *) == NULL)
 			printf("(nil)");
 		else
 			printf("%s", va_arg(ap, char *));
