15197eb5bca4c7f1f5b2fbfbc83c4630bbb71998
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-20 14:16:59 +0000

    Task 1. To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (separator == NULL)
 		{
-			printf("%d", va_arg(ap, int));
+			printf("%d ", va_arg(ap, int));
 		}
 		else
 		{
