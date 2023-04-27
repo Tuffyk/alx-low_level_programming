4523a4b144e6c824db16fcb9b17e2a50cf3f6cce
Taoufik Malki <taoufik.malki@gmail.com>
2023-04-27 22:05:46 +0000

    Task 0.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: list_t pointer
+ *
+ * Return: number of node.
+ */
+
+size_t print_list(const list_t *h)
+{
+
