4ce02e7fd60c4394bf95549dfb26a16d28342d43
Taoufik Malki <taoufik.malki@gmail.com>
2023-09-28 15:50:42 +0000

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int len;
-
-	len = 0;
-	if (*s != '\0')
-	{
-		len += _strlen_recursion(s + 1);
-	}
-	return (len);
+	if (*s == '\0')
+		return (0);
+	else
+		Return = 1 + _strlen_recursion(s + 1);
 }
