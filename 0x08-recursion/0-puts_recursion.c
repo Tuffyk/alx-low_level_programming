fc24bc829e89bf169f01e57f2b5cda91d19fc3c7
Taoufik Malki <taoufik.malki@gmail.com>
2023-09-28 13:41:57 +0000

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * followed by a new line
+ * @s: String pointer
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s == NULL)
+		return (0);
+	if (s != '\0')
+	{
+		_putchar(s + i);
+		i++;
+		_puts_recursion(s + i);
+	}
+}
