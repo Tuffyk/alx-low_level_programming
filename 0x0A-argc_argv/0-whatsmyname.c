24862e5ae32665d39997d7cc191c9b422a9ef8f1
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-02 21:59:16 +0000

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Prints its name followed by new line
@@ -9,7 +10,6 @@
 
 int main(int argc, char *argv[])
 {
-	_putchar(argv[0]);
-	_putchar('\n');
+	printf("%s\n", argv[0]);
 	return (0);
 }
