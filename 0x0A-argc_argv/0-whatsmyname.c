44cda367ad0bb2d20dc87423e5e98f275399c92b
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-02 21:54:41 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints its name followed by new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+	return (0);
+}
