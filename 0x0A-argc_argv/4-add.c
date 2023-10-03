456a37df0ed96db1eb85492fedd354d12e1a457d
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-03 14:27:15 +0000

    Task 4 update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,26 +12,26 @@
 
 int main(int argc, char *argv[])
 {
-	int sum, i;
-
+	int sum, i,j;
+	
 	sum = 0;
 	if (argc <= 1)
-		printf("0\n");
-	else
 	{
-		for (i = 1; i < argc; i++)
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (isdigit(argv[i]))
-			{
-				sum += atoi(argv[i]);
-			}
-			else
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
+			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
