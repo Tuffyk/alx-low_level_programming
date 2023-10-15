0e22de56ef583d9e60e642e3eab04914644a416d
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-15 15:43:09 +0000

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.C
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer of type struct dog
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
