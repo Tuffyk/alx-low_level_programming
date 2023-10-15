b948f8acd885b0c420be70e22dbd1feddc20b0d1
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-15 13:34:38 +0000

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -13,7 +13,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (*d == NULL)
+	if (d == NULL)
 		return (NULL);
 	d->name = name;
 	d->age = age;
