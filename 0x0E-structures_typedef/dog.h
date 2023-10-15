087ab743a95c308be429b3e37af04927899d4745
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-15 12:05:58 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: defines three dog information which are
+ * name, age and owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
