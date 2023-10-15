17672f13fac8611c988468c0533f19c2eb8c0b3a
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-15 13:58:12 +0000

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints dog information
+ * @d: pointer of type struct dog
+ *
+ * Return: void.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("(nil)\n");
+		if (d->age != NULL)
+			printf("Age: %.lf\n", d->age);
+		else
+			printf("(nil)\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("(nil)\n");
+	}
+}
