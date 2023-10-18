64ec302b516ac722e68bb8db3dad8f0120d26b22
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-18 12:58:46 +0000

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array: array of integers
+ * @size: size of the array
+ * @action: function pointer
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
