115a8ce47c862374adb1b653fdd4b37e8a50e8db
Taoufik Malki <taoufik.malki@gmail.com>
2023-10-23 14:54:54 +0000

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,10 @@ char *str_concat(char *s1, char *s2)
 	char *ns;
 	int i, j, lens1, lens2, len;
 
+	if (s1 == NULL)
+		s1[0] = "";
+	if (s2 == NULL)
+		s2[0] = "";
 	if (s1 != NULL)
 	{
 		for (lens1 = 0; s1[lens1] != '\0'; lens1++)
